43c8da0a716d79f01f81f0a4233b5191bd328b09
paul <paul.szelesi@veeam.com>
2019-08-29 11:33:18 +0100

    change

--- conf.py
+++ conf.py
@@ -10,8 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+ import os
+ import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -26,14 +26,30 @@ release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
-#Change Master doc to look for index not master_doc
-# master_doc = 'index.rst'
 
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The encoding of source files.
+#source_encoding = 'utf-8-sig'
+
+# The master toctree document.
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
 
 
 
