01558d80fb6559e1fa935f225326a9a00870fe5e
paul <paul.szelesi@veeam.com>
2019-08-30 08:55:59 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -51,52 +51,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "classic"
+html_theme = "alabaster"
 
 #Options for the them
 html_theme_options = {
-"rightsidebar": False,
-# (True): Put the sidebar on the right side. Defaults to False.
-"stickysidebar": True,
-#(true or false): Make the sidebar “fixed” so that it doesn’t scroll out of view for long body content. This may not work well with all browsers. Defaults to False.
-"collapsiblesidebar": True,
-#(true or false): Add an experimental JavaScript snippet that makes the sidebar collapsible via a button on its side. Defaults to False.
-"externalrefs": True,
-#(true or false): Display external links differently from internal links. Defaults to False.
-"footerbgcolor": 'white',
-# (CSS color): Background color for the footer line.
-"footertextcolor ": 'darkslategrey',
-#(CSS color): Text color for the footer line.
-"sidebarbgcolor": 'white',
-# (CSS color): Background color for the sidebar.
-"sidebarbtncolor": 'darkslategrey',
-#(CSS color): Background color for the sidebar collapse button (used when collapsiblesidebar is True).
-"sidebartextcolor": 'darkgreen',
-#(CSS color): Text color for the sidebar.
-"sidebarlinkcolor":'green',
-#(CSS color): Link color for the sidebar.
-"relbarbgcolor": 'darkslategrey',
-#(CSS color): Background color for the relation bar.
-"relbartextcolor": 'white',
-#(CSS color): Text color for the relation bar.
-"relbarlinkcolor": 'white',
-#(CSS color): Link color for the relation bar.
-"bgcolor": 'white',
-#(CSS color): Body background color.
-"textcolor": 'black',
-#(CSS color): Body text color.
-"linkcolor": 'darkgreen',
-#(CSS color): Body link color.
-"visitedlinkcolor": 'darkgreen',
-#(CSS color): Body color for visited links.
-"headbgcolor": 'white',
-# (CSS color): Background color for headings.
-#headtextcolor (CSS color): Text color for headings.
-#headlinkcolor (CSS color): Link color for headings.
-#codebgcolor (CSS color): Background color for code blocks.
-#codetextcolor (CSS color): Default text color for code blocks, if not set differently by the highlighting style.
-#bodyfont (CSS font-family): Font for normal text.
-#headfont (CSS font-family): Font for headings.
+
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
