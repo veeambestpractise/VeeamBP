6068be5f18cc90bca789285774f0e03587cbf7f9
paul <paul.szelesi@veeam.com>
2019-08-30 13:54:01 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -38,7 +38,7 @@ source_suffix = {
 }
 
 
-html_logo = 'docs/logo.gif'
+html_logo = 'docs/logo_1.jpg'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
