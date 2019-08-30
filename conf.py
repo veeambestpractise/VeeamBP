5c74800c03a64ad056c17581ebd2c5f834d26591
paul <paul.szelesi@veeam.com>
2019-08-30 11:32:27 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -59,7 +59,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "scrolls"
 
 #Options for the them
 html_theme_options = {
