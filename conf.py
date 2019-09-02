1cc5ec229fc2f2672bbe357126f6e8b1c3734f79
paul <paul.szelesi@veeam.com>
2019-09-02 14:49:56 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme_path = ["docs/_themes", ]
 html_theme = "sphinx_rtd_theme"
 
